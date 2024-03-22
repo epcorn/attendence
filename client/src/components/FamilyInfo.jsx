1fa1450883f6140baa7972366f4e495031f5ba08
Kamlesh Pal <kamleshiit5@gmail.com>
2024-03-22 08:04:39 +0530

    worked on input of empployee

--- client/src/components/FamilyInfo.jsx
+++ client/src/components/FamilyInfo.jsx
@@ -1,41 +1,103 @@
-/* eslint-disable react/prop-types */
-import { Label, TextInput } from 'flowbite-react';
+import { Label, TextInput } from "flowbite-react";
+import { useState } from "react";
 
-function FamilyInfo({ formData, setFormData }) {
-    return (
-        <div className="  personal-info-container">
-            <div>
-                <div className=" block">
-                    <Label htmlFor="firstname" value="Name" />
-                </div>
-                <TextInput id="firstname" type="text" required value={formData.firstname} onChange={(e) => setFormData({ ...formData, firstname: e.target.value })} />
-            </div>
-            <div>
-                <div className=" block">
-                    <Label htmlFor="lastname" value="Lastname" />
-                </div>
-                <TextInput id="lastname" type="text" required value={formData.lastname} onChange={(e) => setFormData({ ...formData, lastname: e.target.value })} />
-            </div>
-            <div>
-                <div className=" block">
-                    <Label htmlFor="relation" value="Relation" />
-                </div>
-                <TextInput id="relation" type="text" required value={formData.lastname} onChange={(e) => setFormData({ ...formData, lastname: e.target.value })} />
-            </div>
-            <div>
-                <div className=" block">
-                    <Label htmlFor="email" value="Your email" />
-                </div>
-                <TextInput id="email" placeholder="name@company.com" required />
-            </div>
-            <div>
-                <div className=" block">
-                    <Label htmlFor="phone" value="Phone No" />
-                </div>
-                <TextInput id="phone" placeholder="" />
-            </div>
-        </div>
+const FamilyInfo = () => {
+  const [activeMember, setActiveMember] = useState(0);
+  const [familyMembers, setFamilyMembers] = useState([
+    { id: 1, firstname: "", lastname: "", relation: "", email: "", phone: "" },
+    { id: 2, firstname: "", lastname: "", relation: "", email: "", phone: "" },
+    { id: 3, firstname: "", lastname: "", relation: "", email: "", phone: "" },
+    { id: 4, firstname: "", lastname: "", relation: "", email: "", phone: "" },
+  ]);
+
+  const handleMemberClick = (index) => {
+    setActiveMember(index);
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFamilyMembers((state) =>
+      state.map((element, idx) =>
+        idx === activeMember ? { ...element, [name]: value } : element
+      )
     );
-}
+  };
+
+  return (
+    <div className="">
+      <div className="flex space-x-2 justify-center items-center">
+        {familyMembers.map((member, index) => (
+          <button
+            key={member.id}
+            onClick={() => handleMemberClick(index)}
+            className={`rounded-full w-10 h-10 bg-blue-400 text-white focus:outline-none ${
+              activeMember === index ? "bg-blue-800" : ""
+            }`}
+          >
+            {member.id}
+          </button>
+        ))}
+      </div>
+      <div className=" mb-2">
+        <Label className="font-bold" htmlFor="firstname" value="First name" />
+        <TextInput
+          id="firstname"
+          type="text"
+          required
+          name="firstname"
+          className="felx-2"
+          value={familyMembers[activeMember].firstname}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className=" mb-2">
+        <Label className="font-bold" htmlFor="lastname" value="Lastname" />
+        <TextInput
+          className=""
+          id="lastname"
+          type="text"
+          name="lastname"
+          required
+          value={familyMembers[activeMember].lastname}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className=" mb-2">
+        <Label className="font-bold" htmlFor="relation" value="Relationship" />
+        <TextInput
+          className=""
+          id="relation"
+          type="text"
+          name="relation"
+          required
+          value={familyMembers[activeMember].relation}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className=" mb-2">
+        <Label className="font-bold" htmlFor="email" value="Your email" />
+        <TextInput
+          id="email"
+          placeholder="name@company.com"
+          required
+          name="email"
+          value={familyMembers[activeMember].email}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className=" mb-2">
+        <Label className="font-bold" htmlFor="tel" value="Phone" />
+        <TextInput
+          id="tel"
+          placeholder="Phone"
+          required
+          name="phone"
+          value={familyMembers[activeMember].phone}
+          onChange={handleInputChange}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default FamilyInfo;
