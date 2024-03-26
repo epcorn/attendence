3f10cd98f3c36a76b67357869c1664052f32e0e3
epcorn <exteam.epcorn@gmail.com>
2024-03-26 11:08:44 +0530

    first commit

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -4,7 +4,6 @@ import { createToken } from "../middleware/verifyUser.js";
 
 const login = async (req, res, next) => {
   const { email, password } = req.body;
-
   if (!email || !password || email === "" || password === "") {
     return next(errorHandler(400, "All fields are requird"));
   }
