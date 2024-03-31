8095e8e825f7d22ab320b33ca4943a7aac531757
Kamlesh Pal <kamleshiit5@gmail.com>
2024-03-31 23:46:33 +0530

    mega report :)

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      "/api/v1": {
+      "/api/": {
         target: "http://localhost:5000",
         changeOrigin: true,
-      }
-    }
+      },
+    },
   },
   plugins: [react()],
 });
